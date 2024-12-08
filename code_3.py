cb0646b196329929be2b02ec2b35278334e471bb
genfilweb <142978168+genfilweb@users.noreply.github.com>
2024-12-08 15:40:55 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+        console.log("Random Number: " + item);
+    });
+    const data = generateRandomData();
+}
+    }
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+
+    const data = [];
+
+main();
+}
+
+    return data;
+    data.forEach(item => {
